266bdccfec039faffe1e9bdee9af329762cf1ba4
Zacharias Göransson <zacharias.goransson@student.kyh.se>
2020-09-10 15:46:35 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=1; i<=100; i++){
+
+            if(i==42) {
+                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
+            }
+            // Andreas hintade om att vi skulle använda modulus, samt både else if och else.
+            else if((i%3==0) & (i%5==0)){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i%5==0){
+                System.out.println("Buzz");
+            }
+
+            else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
